44a40cb0dd039275432df42ef05577b5edee5b09
Hirosh07 <94898137+Hirosh07@users.noreply.github.com>
2022-01-09 14:48:21 +0530

    my cap code 2
    .

---
+++ Mycap2.py
@@ -0,0 +1,13 @@
+#1st input
+list1 = [12,-7,5,64,-14]
+for num in list1:
+    if num >= 0:
+       print(num, end = " ")
+       
+       
+       
+#2nd input
+list1 = [12,14,-95,3]
+for num in list1:
+    if num >= 0:
+       print(num, end = " ")
